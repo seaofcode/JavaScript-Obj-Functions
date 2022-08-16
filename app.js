2edd7edd9f9c56534fd64c620f2270def037342a
Matthew J Pearson <seaofcode@gmail.com>
2022-08-16 09:10:51 -0500

    Update app.js
    this keyword Basics

---
+++ app.js
@@ -0,0 +1,20 @@
+// this 
+// points to the left of the dot
+
+// const gary = {
+//     firstName: 'gary',
+//     lastName: 'gygax',
+//     fullName: function(){
+//         console.log(`My full name is ${this.firstName} ${this.lastName}`);
+//     },
+// };
+// const john = {
+//     firstName: 'john',
+//     lastName: 'mac',
+//     fullName: function(){
+//         console.log('My full name is john mac');
+//     },
+// };
+
+// gary.fullName();
+// john.fullName();
