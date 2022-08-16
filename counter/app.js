36070f6377dbe6fd104259daa00b9a6b1d44039a
Matthew J Pearson <seaofcode@gmail.com>
2022-08-16 15:12:09 -0500

    Update app.js
    Counter Constructor, getElement Function

---
+++ counter/app.js
@@ -0,0 +1,22 @@
+function Counter(element, value){
+    this.counter = element;
+    this.value = value;
+    this.resetBtn = element.querySelector('.reset');
+    this.increaseBtn = element.querySelector('.increase');
+    this.decreaseBtn = element.querySelector('.decrease');
+    this.valueDOM = element.querySelector('.value');
+    this.valueDOM.textContent = this.value;
+}
+
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 200);
+
+function getElement(selection){
+    const element = document.querySelector(selection);
+    console.log(element);
+    if(element){
+        return element;
+    }
+    throw new Error(`Please check "${selection}" selector, no such element exists`);
+}
+
