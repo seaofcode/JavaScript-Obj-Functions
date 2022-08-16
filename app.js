3014cd2faa12ffc82b45f9077d0984509c7b3f2c
Matthew J Pearson <seaofcode@gmail.com>
2022-08-16 09:22:35 -0500

    Update app.js
    this - advanced

--- app.js
+++ app.js
@@ -18,3 +18,34 @@
 
 // gary.fullName();
 // john.fullName();
+
+
+/********************** */
+
+// In Reg Functions -not arrow- "this"
+// determined by HOW the function is invoked -left of
+
+// defaults to global window
+// arrow functions - pump the breaks
+
+// function showThis(){
+//     console.log(this);
+// }
+
+// const jojo = {
+//     name: 'jojo',
+//     showThis: showThis,
+// };
+// const dood = {
+//     name: 'dood',
+//     showThis: showThis,
+// };
+
+// // If the object invokes this, calls that object
+// jojo.showThis();
+// dood.showThis();
+
+// // if nothing is invoked left of the dot, the window object is called
+// showThis();
+
+
