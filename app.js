85f2bcdabbd99ce62e907759435c8612318af801
Matthew J Pearson <seaofcode@gmail.com>
2022-08-16 12:26:29 -0500

    HTML, JS
    Call, Apply and Bind

--- app.js
+++ app.js
@@ -157,3 +157,16 @@
 // john.greet.call(sue);
 
 
+// const counter = {
+//     count: 0,
+//     increment() {
+//         console.log(this);
+//         this.count++;
+//         console.log(this.count);
+//     },
+// };
+
+// const btn = document.querySelector('.increment');
+
+// const increment = counter.increment.bind(counter);
+// btn.addEventListener('click', increment);
